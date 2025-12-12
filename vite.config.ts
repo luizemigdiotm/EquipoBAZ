f6b642ff93d681f17de5a87b69b5e7f422b9e9b7
luizemigdiotm <luisemigdiotiradomartinez@gmail.com>
2025-12-11 20:04:03 -0600

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/equipobaz/",
   resolve: {
     alias: [
       // Changed from '@' to '@/' to avoid conflicts with npm packages starting with @ (like @supabase)
       { find: '@/', replacement: '/src/' }
+
     ]
   },
   build: {
